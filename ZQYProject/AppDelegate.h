a0150b732219c3890d773eea32c3b60bfcd3d18b
zhangqingyu <1729810992@qq.com>
2019-10-10 11:00:47 +0800

    Initial Commit

---
+++ ZQYProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQYProject
+//
+//  Created by  张清玉 on 2019/10/10.
+//  Copyright © 2019 zqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQYProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ZQYProject
+//
+//  Created by  张清玉 on 2019/10/10.
+//  Copyright © 2019 zqy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
